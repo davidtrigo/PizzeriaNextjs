bdfac5d0d34bfa240131c8edb4927b449d29a1a9
davidtrigo <d.trigo83@gmail.com>
2020-06-08 16:15:50 +0200

    created menu drawer responsive

--- components/appbar.js
+++ components/appbar.js
@@ -3,34 +3,42 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-import User from './user'
-import Search from './search'
-import Drawer from './drawer'
+import { drawerWidth } from './menu'
+import User from '../components/user'
+//import Search from '../components/search'
+
+import { useContext } from 'react'
+import MenuContext from './menucontext';
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
+   
     menuButton: {
         marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    appBar: {
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
     },
 }));
 export default function appBar() {
+    const { handleDrawerToggle } = useContext(MenuContext);
     const classes = useStyles()
-    return (<div className={classes.root}>
-        <AppBar position="static">
+    return (
+        <AppBar position="fixed" className={classes.appBar}>
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    
+                <IconButton edge="start" className={classes.menuButton}
+                    color="inherit" aria-label="menu"
+                    onClick={handleDrawerToggle}
+                >
                     <MenuIcon />
-                     
                 </IconButton>
-              
-                Pizzeria
-                <Search></Search>
                 <User></User>
-          
             </Toolbar>
         </AppBar>
-    </div>
+  
     )
 }
--- pages/ingredients.js
+++ pages/ingredients.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 import User from '../components/user'
 import AppBar from '../components/appbar'
 import Layout from '../components/layout'
-export default function Home() {
+export default function Ingredients() {
   return (
     <>
       <Head>
         <title>Pizzeria</title>
       </Head>
       <Layout>
-        <div>Estas en Pizza</div>
+        <div>Estas en Ingredientes</div>
       </Layout>
     </>
   )
