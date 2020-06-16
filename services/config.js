231443233bcd7f90a8915965b946fb747915065c
davidtrigo <d.trigo83@gmail.com>
2020-06-16 12:38:28 +0200

    fixed

--- services/config.js
+++ services/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     cloudName: 'djevquc9d',
     apiKey: '724348192427348',
+    algoliaApplicationID:"0JG7DBLM8M",
+    algoliaSearchID:"668e6e13a2aeef444415a20423f904be",
+
     
 };
